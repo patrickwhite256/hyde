281b97601c40f186b85e2a36c5e42899631262e5
Patrick White <patrickwhite.256@gmail.com>
2015-01-24 22:36:49 -0500

    Everything is prettier with bootstrap

--- hyde-django/hyde_web/views.py
+++ hyde-django/hyde_web/views.py
@@ -5,3 +5,6 @@ from django.views.generic import TemplateView
 
 class InputView(TemplateView):
     template_name = "input.html"
+    def get(self, request, *args, **kwargs):
+        print(request)
+        return super().get(request, *args, **kwargs)
