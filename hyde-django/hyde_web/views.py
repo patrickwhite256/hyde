2a7df8a3d37968328a3d4bfc00b2940293a21cc3
Patrick White <patrickwhite.256@gmail.com>
2015-01-24 23:10:55 -0500

    Mostly finished input

--- hyde-django/hyde_web/views.py
+++ hyde-django/hyde_web/views.py
@@ -5,6 +5,15 @@ from django.views.generic import TemplateView
 
 class InputView(TemplateView):
     template_name = "input.html"
-    def get(self, request, *args, **kwargs):
-        print(request)
-        return super().get(request, *args, **kwargs)
+    warning = None
+    def post(self, request, *args, **kwargs):
+        if 'hidefile' in request.FILES and 'hiddenfile' in request.FILES:
+            pass
+        elif 'jekfile' in request.FILES:
+            pass
+        return self.get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['warning'] = self.warning
+        return context
